e98c3b704e4df44f96534fc169257df2d361bf30
--Explosion-- <explosion.scratch@gmail.com>
2022-12-27 07:31:22 -0600

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Thomas Stafford on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
